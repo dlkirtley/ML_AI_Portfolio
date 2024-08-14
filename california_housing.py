e596dcf7f40bbf232b697f2fa9914a2744c7b284
dlkirtley <dakotakirtley96@gmail.com>
2024-08-13 20:37:01 -0400

    New Changes

--- california_housing.py
+++ california_housing.py
@@ -1,9 +1,21 @@
 import numpy
+import pandas
+from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
 from sklearn import datasets
+from sklearn.model_selection import train_test_split as tts
+
 
 data = datasets.fetch_california_housing(as_frame=True)
-data = data.frame
-print(data.head())
+calihousing = data.frame
 
-test = 2*5
+X = calihousing.drop('MedHouseVal',axis = 1)
+y = calihousing['MedHouseVal']
+
+Xtrain, Xtest, ytrain, ytest = tts(X,y,test_size = 0.2,random_state = 42)
+scatter_matrix (calihousing,alpha=0.2, figsize=(12,8),diagonal = 'kde')
+plt.show()
+print(X.head())
+
+
+
