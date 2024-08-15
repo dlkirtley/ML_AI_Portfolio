31c055acefcacf4946327dd3c5988284d8baac5b
dlkirtley <dakotakirtley96@gmail.com>
2024-08-15 19:32:21 -0400

    Update hr.scrape.py

--- hr.scrape.py
+++ hr.scrape.py
@@ -1,5 +1,5 @@
 import numpy
-import pandas
+import pandas as pd
 from bs4 import BeautifulSoup
 import requests
 import lxml
@@ -26,43 +26,53 @@ if table:
     # Find all rows in the table
     rows = table.find_all('tr')
 
-    #Lists to store player names and id numbers
-    names = []
-    player_ids = []
-
-    # Iterate through rows
-    for row in rows:
-        # Find all <span> elements with the specific class inside the row
-        spans = row.find_all('span', class_='full-G_bAyq40')
-        name = ' '.join(span.text.strip() for span in spans)
-        if name != '':
-            names.append(name.lower())
-        # Find all <a> elements with the specific class inside the row
-        links = row.find_all('a', class_='bui-link')
-        
-        
-        for link in links:
-            # Extract the href attribute
-            href = link.get('href', '')
-
-            # Check if the href contains '/player/'
-            if '/player/' in href:
-                # Extract player ID by splitting the href
-                player_id = href.split('/player/')[1].split('/')[0]
-                if player_id != '':
-                    player_ids.append(player_id)
-
-    #Check that lengths of names list and player_id list are the same
-    
-    if len(names)!=len(player_ids):
-         raise ValueError(f"Lists are not of the same length: Names({len(names)}) != Player IDs({len(player_ids)})")
-    else:
-        print('success')
-
 else:
     print("No table found on the page.")
+    raise ValueError(f"No Table Found")
 
-    #test
+#Lists to store player names and id numbers
+names = []
+player_ids = []
+
+# Iterate through rows
+for row in rows:
+    # Find all <span> elements with the specific class inside the row
+    spans = row.find_all('span', class_='full-G_bAyq40')
+    name = ' '.join(span.text.strip() for span in spans)
+    if name != '':
+        names.append(name.lower())
+    # Find all <a> elements with the specific class inside the row
+    links = row.find_all('a', class_='bui-link')
+    
+    
+    for link in links:
+        # Extract the href attribute
+        href = link.get('href', '')
+
+        # Check if the href contains '/player/'
+        if '/player/' in href:
+            # Extract player ID by splitting the href
+            player_id = href.split('/player/')[1].split('/')[0]
+            if player_id != '':
+                player_ids.append(player_id)
+
+#Check that lengths of names list and player_id list are the same
+
+if len(names)!=len(player_ids):
+        raise ValueError(f"Lists are not of the same length: Names({len(names)}) != Player IDs({len(player_ids)})")
+else:
+    print('success')
+
+
+df = pd.DataFrame({
+    'Name': names,
+    'ID': player_ids})
+
+print(df.head())
+
+
+
+#test
 
 
 
